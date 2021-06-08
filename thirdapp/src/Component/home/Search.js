b28ba8a0893080a986bc532b9c60f824e56ab255
aakashdeveloper <ahanda205@gmail.com>
2021-06-08 19:43:53 +0530

    apicall

---
+++ thirdapp/src/Component/home/Search.js
@@ -0,0 +1,106 @@
+import React, {Component} from 'react';
+import './Search.css';
+
+const cityUrl = "https://developerfunnel.herokuapp.com/location";
+const hotelUrl = "https://developerfunnel.herokuapp.com/hotels?city="
+
+class Search extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            location:'',
+            hotels:''
+        }
+    }
+
+    renderCity = (data) => {
+        if(data){
+            return data.map((item) => {
+                return(
+                    <option value={item._id}>
+                        {item.city_name}
+                    </option>
+                )
+            })
+        }
+    }
+
+    renderHotel = (data) => {
+        if(data){
+            return data.map((item) => {
+                return (
+                    <option value={item._id}>
+                        {item.name} | {item.locality}
+                    </option>
+                )
+            })
+        }
+    }
+
+    handleCity = (event) => {
+        console.log(event.target.value)
+        const cityId = event.target.value;
+        fetch(`${hotelUrl}${cityId}`,{method:'GET'})
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            this.setState({hotels:data})
+        })
+    }
+
+    render(){
+        // console.log("in render>>>",this.state.location)
+        return(
+            <div className="imageContainer">
+                <div id="logo">
+                    D!
+                </div>
+                <div className="heading">
+                    Plan Trip With Us
+                </div>
+                <div className="locationSelector">
+                    <select className="locationDropDown" onChange={this.handleCity}>
+                        <option>-----SELECT CITY------</option>
+                        {this.renderCity(this.state.location)}
+                    </select>
+                    <select className="hotelDropDown">
+                        <option>-----SELECT HOTEL------</option>
+                        {this.renderHotel(this.state.hotels)}
+                    </select>
+                </div>
+            </div>
+        )
+    }
+
+    // on page load call api 
+    componentDidMount(){
+        //we get data and update the state
+        fetch(cityUrl,{method:'GET'})
+        // return the promise here
+        .then((res) => res.json())
+        // resolve the promise and get data
+        .then((data) => {
+            console.log(data)
+            // setting in state
+            this.setState({location:data})
+        })
+        // error handling
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+}
+
+
+export default Search
+
+/*
+constructor
+render
+renderCity > it will not run as we don't have data
+api 
+set data in state
+renderCity > we have data
+>>it load option in dropdown
+*/
